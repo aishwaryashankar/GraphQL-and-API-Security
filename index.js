fb2ffdcf5e9dbefc2f3f7ad96e6789225ed68942
Aishwarya <aishwaryashankar17@gmail.com>
2024-11-10 16:47:19 -0800

    Basic resolvers created

--- index.js
+++ index.js
@@ -10,6 +10,41 @@ import db from './db.js'
 import { typeDefs } from './schema.js'
 
 // Resolvers
-
+const resolvers = {
+    Query: {
+        users() 
+        {
+            return db.users
+        },
+        recipes() 
+        {
+            return db.recipes
+        },
+        reviews() 
+        {
+            return db.reviews
+        },
+        user(_,args) 
+        {
+            return db.users.find((user) => user.id = args.id)
+        },
+        recipe(_,args)
+        {
+            return db.recipes.find((recipe) => recipe.id = args.id)
+        },
+        review(_,args)
+        {
+            return db.reviews.find((review) => review.id = args.id)
+        }
+    }
+}
 
 // Server
+const server = new ApolloServer(
+    {
+        typeDefs,
+        resolvers
+    }
+)
+const { url } = await startStandaloneServer(server, {listen: { port: 4000 }})
+console.log(`The Apollo Server is up and running at port ${url}`)
