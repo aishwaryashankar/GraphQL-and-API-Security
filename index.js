9a28df4574bb6cd4442d9d0d999b3819af1f0319
Aishwarya <aishwaryashankar17@gmail.com>
2024-11-10 17:28:21 -0800

    Added more resolvers

--- index.js
+++ index.js
@@ -12,29 +12,62 @@ import { typeDefs } from './schema.js'
 // Resolvers
 const resolvers = {
     Query: {
+        // retrieve all users
         users() 
         {
             return db.users
         },
+
+        // retrieve all recipes
         recipes() 
         {
             return db.recipes
         },
+
+        // retrieve all recipe reviews
         reviews() 
         {
             return db.reviews
         },
+
+        // retrieve a single user based on id
         user(_,args) 
         {
-            return db.users.find((user) => user.id = args.id)
+            return db.users.find((user) => user.id === args.id)
         },
+
+        // retrieve a single recipe based on id
         recipe(_,args)
         {
-            return db.recipes.find((recipe) => recipe.id = args.id)
+            return db.recipes.find((recipe) => recipe.id === args.id)
         },
+
+        // retrieve a single recipe review based on id
         review(_,args)
         {
-            return db.reviews.find((review) => review.id = args.id)
+            return db.reviews.find((review) => review.id === args.id)
+        }
+    },
+
+    // Given a recipe, find all the reviews for that recipe
+    Recipe:
+    {
+        reviews(parent) 
+        {
+            return db.reviews.filter((review) => review.recipe_id === parent.id)
+        }
+    },
+
+    // Given a recipe review, find the recipe it is for and the user who posted the review
+    Review: 
+    {
+        recipe(parent)
+        {
+            return db.recipes.find((recipe) => recipe.id === parent.recipe_id)
+        },
+        user(parent)
+        {
+            return db.users.find((user) => user.id === parent.user_id)
         }
     }
 }
